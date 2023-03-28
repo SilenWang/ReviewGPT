b2b7e9977dd89630924203fad8d55ccbe0e22aef
Silen Wang <qiumin14@163.com>
2023-03-28 21:44:37 +0800

    文章阅读用函数实现

--- utils/review.py
+++ utils/review.py
@@ -2,7 +2,9 @@
 
 # import tiktoken
 import openai
+from openai.embeddings_utils import get_embedding, cosine_similarity
 from json import dump, loads
+import pandas as pd
 
 try:
     from utils.config import Prompts, OPENAI_KEY, REVIEW_MODEL
@@ -89,6 +91,31 @@ class Reviewer:
         return self.query()
 
 
+    def review(self, question: str, paper_data: pd.DataFrame, top: int = 2):
+        '''
+        阅读文献的部分内容, 给出问题的回答
+        step1, 根据问题, 计算embedding
+        step2, 找到top N 最接近的内容
+        step3, 形成prompts, 请求回答
+        '''
+        question_embedding = get_embedding(question, engine='text-embedding-ada-002')
+        paper_data['similarity'] = paper_data['embedding'].apply(lambda x: cosine_similarity(x, question_embedding))
+        # 找出最符合的n段文本, 默认2, 主要是为了节省token...
+        top_texts = paper_data.sort_values(by='similarity', ascending=False).head(2)[['page', 'text']].values.tolist()
+
+        units = []
+        for page, text in top_texts:
+            units.append(Prompts['Review_Unit'].format(page=page, content=text))
+        
+        message = Prompts['Review'].format(pages='\n'.join(units), question=question)
+
+        self.messages = [
+            {"role": "user", "content": message},
+        ]
+
+        return self.query()
+
+
 def screen_demo():
     reviewer = Reviewer()
     criterias = '''
@@ -117,10 +144,12 @@ def screen_demo():
         dump(answer, aJson)
 
 
-if __name__ == '__main__':
+def summarise_demo():
     reviewer = Reviewer()
     papers = [
         ('12345', 'Background: Little is known about reproductive health in severely obese women. In this study, we present associations between different levels of severe obesity and a wide range of health outcomes in the mother and child. Method(s): From the Danish National Birth Cohort, we obtained self-reported information about prepregnant body mass index (BMI) for 2451 severely obese women and 2450 randomly selected women from the remaining cohort who served as a comparison group. Information about maternal and infant outcomes was also self-reported or came from registers. Logistic regression was used to estimate the association between different levels of severe obesity and reproductive outcomes. Principal Findings: Subfecundity was more frequent in severely obese women, and during pregnancy, they had an excess risk of urinary tract infections, gestational diabetes, preeclampsia and other hypertensive disorders which increased with severity of obesity. They tended to have a higher risk of both pre- and post-term birth, and risk of cesarean and instrumental deliveries increased across obesity categories. After birth, severely obese women more often failed to initiate or sustain breastfeeding. Risk of weight retention 1.5 years after birth was similar to that of other women, but after adjustment for gestational weight gain, the risk was increased, especially in women in the lowest obesity category. In infants, increasing maternal obesity was associated with decreased risk of a low birth weight and increased risk of a high birth weight. Estimates for ponderal index showed the same pattern indicating an increasing risk of neonatal fatness with severity of obesity. Infant obesity measured one year after birth was also increased in children of severely obese mothers. Conclusion(s): Severe obesity is correlated with a substantial disease burden in reproductive health. Although the causal mechanisms remain elusive, these findings are useful for making predictions and planning health care at the individual level. © 2009 Nohr et al.'),
         ('45456', 'Background: Preeclampsia is one of the leading causes of maternal and perinatal morbidity and mortality world-wide. The risk for developing preeclampsia varies depending on the underlying mechanism. Because the disorder is heterogeneous, the pathogenesis can differ in women with various risk factors. Understanding these mechanisms of disease responsible for preeclampsia as well as risk assessment is still a major challenge. The aim of this study was to determine the risk factors associated with preeclampsia, in healthy women in maternity hospitals of Karachi and Rawalpindi. Method(s): We conducted a hospital based matched case-control study to assess the factors associated with preeclampsia in Karachi and Rawalpindi, from January 2006 to December 2007. 131 hospital-reported cases of PE and 262 controls without history of preeclampsia were enrolled within 3 days of delivery. Cases and controls were matched on the hospital, day of delivery and parity. Potential risk factors for preeclampsia were ascertained during in-person postpartum interviews using a structured questionnaire and by medical record abstraction. Conditional logistic regression was used to estimate matched odds ratios (ORs) and 95% confidence intervals (95% CIs). Result(s): In multivariate analysis, women having a family history of hypertension (adjusted OR 2.06, 95% CI; 1.27-3.35), gestational diabetes (adjusted OR 6.57, 95% CI; 1.94 -22.25), pre-gestational diabetes (adjusted OR 7.36, 95% CI; 1.37-33.66) and mental stress during pregnancy (adjusted OR 1.32; 95% CI; 1.19-1.46, for each 5 unit increase in Perceived stress scale score) were at increased risk of preeclampsia. However, high body mass index, maternal age, urinary tract infection, use of condoms prior to index pregnancy and sociodemographic factors were not associated with higher risk of having preeclampsia. Conclusion(s): Development of preeclampsia was associated with gestational diabetes, pregestational diabetes, family history of hypertension and mental stress during pregnancy. These factors can be used as a screening tool for preeclampsia prediction. Identification of the above mentioned predictors would enhance the ability to diagnose and monitor women likely to develop preeclampsia before the onset of disease for timely interventions and better maternal and fetal outcomes. © 2010 Shamsi et al; licensee BioMed Central Ltd.'),
     ]
     reviewer.summarise(papers)
+
+
