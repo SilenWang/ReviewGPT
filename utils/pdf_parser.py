afec1197b9fe5d050b9eeae0a9de89cf898d43f3
Silen Wang <qiumin14@163.com>
2023-04-01 15:10:55 +0800

    当页面上没有内容时, 不存储到数据框, 防止后续解析出问题

--- utils/pdf_parser.py
+++ utils/pdf_parser.py
@@ -10,9 +10,9 @@ except ImportError:
 
 class PdfFile:
     '''
-    pdf文件解析器, 使用pdfplumber模块, 目前是个简单的实现, 之后需要找
-    有没有什么更好的ML模块能更好的提取文章内容(精确到节段这种, embedding
-    会更好), 决定位置也更精确
+    1. pdf文件解析器, 目前使用pdfplumber模块简单实现
+    2. 之后需要找更好的ML模块以能更好的提取文章内容(精确到节段), 位置定位更精确
+    3. 在有新的提取方式以前, 这部分不添加设置项
     '''
 
     def __init__(self, file, api_key=None):
@@ -30,15 +30,17 @@ class PdfFile:
         一页内容存一次, 所有内容通过模型进行预测
         '''
         all_text = []
-        openai.api_key = self.api_key
         for page in self.pdf_file.pages:
-            all_text.append({
-                'page': page.page_number,
-                'text': page.dedupe_chars().extract_text(layout=True)
-            })
+            page_text = page.dedupe_chars().extract_text(layout=True)
+            if page_text:
+                all_text.append({
+                    'page': page.page_number,
+                    'text': page_text
+                })
         text_data = pd.DataFrame(all_text)
+        openai.api_key = self.api_key
         text_data['embedding'] = text_data['text'].apply(lambda x: get_embedding(x, engine='text-embedding-ada-002'))
-        text_data.to_csv('debug.data.csv', index=0)
+        # text_data.to_csv('debug.data.csv', index=0)
         return text_data
 
 
